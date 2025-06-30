9bc5f862e3bfc3611854918aa617cd83b1fb5131
Odai Bishr <odaibishr@gmail.com>
2025-07-01 00:43:02 +0300

    editing my icon and put it in the header

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import ThemeSwitcher from "../ThemeSwitcher";
+import Image from "next/image";
 
 const usesPathname = () => {
     const pathname = usePathname();
@@ -31,9 +32,11 @@ const Links = [
 export default function Header() {
     const pathname = usesPathname();
     return (
-        <header className="fixed top-0 z-10 py-4 w-full">
+        <header className="fixed top-0 z-10  w-full">
             <nav className="container flex items-center justify-between">
-                <Link href="/" className="text-lg font-bold">Odai Beshr</Link>
+                <Link href="/" className="text-lg font-bold">
+                    <Image src="/logo.png" alt="Logo" width={80} height={80} className=" bg-cover" priority  />
+                </Link>
                 <div className="space-x-4 hidden md:flex">
                     {Links.map((link) => (
                         <Link key={link.href} href={link.href} className={cn("text-foreground group-hover/nav-item:text-primary block duration-150 hover:text-primary",
