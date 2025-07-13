91825871f10827a30d431471d9f32d5861683493
Odai Bishr <odaibishr@gmail.com>
2025-07-14 02:02:22 +0300

    fix the null for project details page

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,4 +1,3 @@
-
 import { client } from "@/sanity/lib/client";
 import { PROJECTS_DETAIL_QUERY } from "@/sanity/queries";
 import { Project } from "@/sanity.types";
@@ -8,10 +7,10 @@ import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
 import MainButton from "@/components/mainButton";
 
-
 export default async function ProjectDetails({ params }: { params: Promise<{ slug: string }> }) {
     const { slug } = await params;
     const project = await client.fetch<Project>(PROJECTS_DETAIL_QUERY, { slug });
+
     return (
         <section className="pt-20 md:pt-28 container mx-auto">
             <Link href={`/projects`} className="flex items-center gap-2">
@@ -19,27 +18,28 @@ export default async function ProjectDetails({ params }: { params: Promise<{ slu
                 <p className="text-muted-foreground">Back to projects</p>
             </Link>
             <div className="mt-4 mb-4">
-                <Image
-                    src={urlFor(project.prjectImage!).url()}
-                    alt={project?.title!}
-                    width={1500}
-                    height={100}
-                    className="rounded-2xl"
-                />
+                {project?.prjectImage && (
+                    <Image
+                        src={urlFor(project.prjectImage).url()}
+                        alt={project.title || ""}
+                        width={1500}
+                        height={100}
+                        className="rounded-2xl"
+                    />
+                )}
             </div>
             <div className="flex flex-col md:flex-row justify-between">
-                <p className="text-2xl font-bold">{project?.title!}</p>
+                <p className="text-2xl font-bold">{project?.title || ""}</p>
                 <div className="mt-4 md:mt-0 flex items-center gap-4">
-                    <MainButton text="View Project" href={`/projects/${project.slug?.current}`} />
-                    <MainButton text="Source Code" href={`/projects/${project.slug?.current}`} />
+                    <MainButton text="View Project" href={`/projects/${project?.slug?.current || ""}`} />
+                    <MainButton text="Source Code" href={`/projects/${project?.slug?.current || ""}`} />
                 </div>
             </div>
             <h2 className="mt-4 text-xl font-bold">Description</h2>
-            <p className="text-muted-foreground mt-2 md:w-[700px]">{project?.description!}</p>
+            <p className="text-muted-foreground mt-2 md:w-[700px]">{project?.description || ""}</p>
             <div>
                 <p className="mt-4 text-xl font-bold">Technologies</p>
                 <div className="flex items-center gap-4 mt-2">
-
                 </div>
             </div>
         </section>
