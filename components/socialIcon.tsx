1bb3bcc7334cfc0ce83e701a92567f9db9b1d616
Odai Bishr <odaibishr@gmail.com>
2025-07-27 03:08:12 +0300

    enhance mobile navigation and footer socailIcon component

--- components/socialIcon.tsx
+++ components/socialIcon.tsx
@@ -1,16 +1,11 @@
 import { Social } from "@/sanity.types";
-import Image from "next/image";
-import { urlFor } from "@/sanity/lib/image";
+import { DynamicIcon } from "lucide-react/dynamic";
+import Link from "next/link";
 
 export default function SocialIcon({ social }: { social: Social }) {
     return (
-        <a href={social.url} >
-            <Image
-                src={urlFor(social.icon!).url()}
-                alt={social.platform!}
-                width={24}
-                height={24}
-                className="transform transition duration-300 ease-in-out hover:scale-110 object-contain" />
-        </a>
+        <Link href={social.url!} className="text-foreground h-12 w-12 flex items-center justify-center group bg-card rounded-full transition-all duration-300 hover:scale-110 hover:-translate-y-1">
+            <DynamicIcon className="size-5 group-hover:text-primary" name={social.platform!.toLowerCase() as any} />
+        </Link>
     )
 }
